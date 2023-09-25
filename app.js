c84fa01d81d691327dac0b0dae71794ab2b4f35b
Tibi-The-Defiler64 <tibi.hobit@gmail.com>
2023-09-25 23:47:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//
+import express from 'express';
+//const express = require("express");
+import fetch from 'node-fetch';
+
+//quire("dotenv").config();
+import 'dotenv/config';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view_engine","ejs");
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+    extended:true,
+}));
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/mp3",async (req,res)=>{
+    const videoId = req.body.videoID;
+
+    if(videoId === undefined || videoId === "" || videoId === null){
+        return res.render("index.ejs",{success : false, message : "Please enter a video ID"});
+    }else{
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,{
+            "method" : "GET",
+            "headers":{
+                "x-rapidapi-key" : process.env.API_KEY,
+                "x-rapidapi-host" : process.env.API_HOST
+            }
+        });
+
+        const fetchResponse = await fetchAPI.json();
+        
+        if(fetchResponse.status === "ok"){
+            
+            return res.render("index.ejs",{success : true, song_title : fetchResponse.title,
+            song_link : fetchResponse.link});
+            }
+        else
+            return res.render("index.ejs", {success: false, message: fetchResponse.msg});
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`);
+
+});
+
+
+
